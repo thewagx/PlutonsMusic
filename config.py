bfd2350934471c210ee68db5c381cf452805bc84
Mr Davids <hellofucking@icloud.com>
2023-10-28 23:23:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/af80ded292afb2352e1ca.png")
 
 SESSION = getenv("SESSION", None)
 
